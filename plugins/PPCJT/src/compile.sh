4f25402acde44ef613430a61b6bf2db907eff422
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2011-05-16 20:39:19 -0400

    Remove useless compile.sh and fix plugin filename in ppcjt.vcproj

--- plugins/PPCJT/src/compile.sh
+++
@@ -1,6 +0,0 @@
-
-g++ -g -D__LINUX__ -D_FORTIFY_SOURCE=0 -fvisibility=hidden -fvisibility-inlines-hidden --shared -D__EA64__ -I$HOME/idasdk/60/module -I$HOME/idasdk/60/include/ -DNO_OBSOLETE_FUNCS -D__IDP__ -pipe -c -o main.o64 main.cpp  && \
-g++ -lrt -lpthread -D__LINUX__ -D_FORTIFY_SOURCE=0 -fvisibility=hidden -fvisibility-inlines-hidden --shared -Wl,--gc-sections -Wl,--no-undefined -o ppcjt.plx64 main.o64 -L$HOME/idasdk/60/bin/ -lida64
-
-g++ -g -D__LINUX__ -D_FORTIFY_SOURCE=0 -fvisibility=hidden -fvisibility-inlines-hidden --shared -I$HOME/idasdk/60/module -I$HOME/idasdk/60/include/ -DNO_OBSOLETE_FUNCS -D__IDP__ -pipe -c -o main.o main.cpp  && \
-g++ -lrt -lpthread -D__LINUX__ -D_FORTIFY_SOURCE=0 -fvisibility=hidden -fvisibility-inlines-hidden --shared -Wl,--gc-sections -Wl,--no-undefined -o ppcjt.plx main.o -L$HOME/idasdk/60/bin/ -lida
